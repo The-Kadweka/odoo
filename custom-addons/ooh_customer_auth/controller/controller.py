64f50779ab458e94cbd7192dd322b194e6b65c00
The-Kadweka <49676297+Kadweka@users.noreply.github.com>
2023-01-21 13:57:49 +0300

    update the user informtion

--- custom-addons/ooh_customer_auth/controller/controller.py
+++ custom-addons/ooh_customer_auth/controller/controller.py
@@ -462,7 +462,7 @@ class MoneyController(http.Controller):
                 category.append({"name":"Electronics","amount":round(electronics,3) if electronics>0 else round(0.00),"per":round(electronics/income*100,1) if electronics>0 else round(0.00)})
                 category.append({"name":"Charity","amount":round(charity,2) if charity>0 else round(0.00),"per":round(charity/income*100,1) if charity>0 else round(0.00)})
                 category.append({"name":"Clothing","amount":round(clothing,2) if clothing>0 else round(0.00),"per":round(clothing/income*100,1) if clothing>0 else round(0.00)})
-                category.append({"name":"Medication","amount":(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
+                category.append({"name":"Medication","amount":round(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
 
                 return{
                     "code":200,
@@ -523,7 +523,7 @@ class MoneyController(http.Controller):
                 category.append({"name":"Electronics","amount":round(electronics,3) if electronics>0 else round(0.00),"per":round(electronics/income*100,1) if electronics>0 else round(0.00)})
                 category.append({"name":"Charity","amount":round(charity,2) if charity>0 else round(0.00),"per":round(charity/income*100,1) if charity>0 else round(0.00)})
                 category.append({"name":"Clothing","amount":round(clothing,2) if clothing>0 else round(0.00),"per":round(clothing/income*100,1) if clothing>0 else round(0.00)})
-                category.append({"name":"Medication","amount":(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
+                category.append({"name":"Medication","amount":round(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
                 return{
                     "code":200,
                     "status":"successfuly",
@@ -589,7 +589,7 @@ class MoneyController(http.Controller):
                 category.append({"name":"Electronics","amount":round(electronics,3) if electronics>0 else round(0.00),"per":round(electronics/income*100,1) if electronics>0 else round(0.00)})
                 category.append({"name":"Charity","amount":round(charity,2) if charity>0 else round(0.00),"per":round(charity/income*100,1) if charity>0 else round(0.00)})
                 category.append({"name":"Clothing","amount":round(clothing,2) if clothing>0 else round(0.00),"per":round(clothing/income*100,1) if clothing>0 else round(0.00)})
-                category.append({"name":"Medication","amount":(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
+                category.append({"name":"Medication","amount":round(meds,2) if meds>0 else round(0.00),"per":round(meds/income*100,1) if meds>0 else round(0.00)})
                 return{
                     "code":200,
                     "status":"successfuly",
